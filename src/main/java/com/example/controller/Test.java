403d6433995f5912398d8e42648c5136cb1c92d0
Anikesh <anikesh@0725@gmail.com>
2021-06-16 11:57:00 +0530

    Addition :Using Query Hello from Bridgelabz

--- src/main/java/com/example/controller/Test.java
+++ src/main/java/com/example/controller/Test.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +14,9 @@ public String sayHello()
 {
 	return "Hello" + "From " + "BridgeLaz";
 }
-	
+
+@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+public String sayHello(@RequestParam (value ="name")String name)
+{
+	return "Hello" + " " +name + "From " + "BridgeLaz";}
 }
